31847cfb4276d927136b8d483c8fab4b5c89b663
Jacob Campbell <jakehawk34@gmail.com>
2020-12-24 14:01:36 +0000

    New imports

--- backend/NYC_API_Export.py
+++ backend/NYC_API_Export.py
@@ -4,8 +4,10 @@ import certifi
 import urllib3
 from urllib3 import request
 import json
-import numpy as np
 from matplotlib import pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsRegressor
 
 http = urllib3.PoolManager(
        cert_reqs='CERT_REQUIRED',
